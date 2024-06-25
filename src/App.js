ebe446bb7fa04e1c39ee89f0cfd0859b722ca743
abraham <abrahamtikuye@gmail.com>
2024-06-25 12:33:05 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSongs } from './songsSlice';
+import SongList from './components/SongList';
+import AddSongForm from './components/AddSongForm';
 
+const App = () => {
+  const dispatch = useDispatch();
+  const songs = useSelector(state => state.songs.songs);
+  const songStatus = useSelector(state => state.songs.status);
 
-import React from 'react'
+  useEffect(() => {
+    if (songStatus === 'idle') {
+      dispatch(fetchSongs());
+    }
+  }, [songStatus, dispatch]);
 
-function App() {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <h1>Song Manager</h1>
+      <AddSongForm />
+      <SongList songs={songs} />
+    </div>
+  );
+};
 
 export default App;
+
+
