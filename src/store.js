5027e8e3741f5f2ba6eb2896877d22caef5ec2e9
abraham <abrahamtikuye@gmail.com>
2024-06-29 10:48:08 +0300

    components based on sagas config

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import songsReducer from './songsSlice';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     songs: songsReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
