934f411cbff80d2762c1e8b2c36f0977e7b5a858
abraham <abrahamtikuye@gmail.com>
2024-06-29 10:35:31 +0300

    saga setup

--- src/components/AddSongForm.js
+++ src/components/AddSongForm.js
@@ -22,7 +22,15 @@ const AddSongForm = () => {
         placeholder="Song title"
         className="w-full p-3 mb-4 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-purple-400"
       />
-          <button className="bg-gradient-to-r from-blue-500 to-red-500 text-white px-4 py-2 text-xl rounded font-medium focus:ring ring-black ring-opacity-10 gradient element-to-rotate">Add song</button>
+      <button
+      className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg bg-gradient-to-tr from-light-blue-600 to-light-blue-400 text-white shadow-md shadow-light-blue-500/20 hover:shadow-lg hover:shadow-light-blue-500/40 active:opacity-[0.85] group relative flex items-center gap-3 overflow-hidden pr-[72px]"
+      type="button">
+      Sign in with Twitter
+      <span class="absolute right-0 grid w-12 h-full transition-colors place-items-center bg-light-blue-600 group-hover:bg-light-blue-700">
+       
+      </span>
+      </button>
+          {/* <button className="bg-gradient-to-r from-blue-500 to-red-500 text-white px-4 py-2 text-xl rounded font-medium focus:ring ring-black ring-opacity-10 gradient element-to-rotate">Add song</button> */}
     </form>
   );
 };
