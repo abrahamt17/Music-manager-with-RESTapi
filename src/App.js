f0ab8413bd5a24b3a1038174ba979325a0938384
abraham <abrahamtikuye@gmail.com>
2024-06-29 10:55:26 +0300

    appconatiner in App.js styling using emotion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSongs } from './songsSlice';
 import SongList from './components/SongList';
 import AddSongForm from './components/AddSongForm';
 
+const AppContainer = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(to bottom right, #6b21a8, #111827, #6b21a8);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+
 const App = () => {
   const dispatch = useDispatch();
-  const songs = useSelector(state => state.songs.songs);
-  const songStatus = useSelector(state => state.songs.status);
+  const songs = useSelector((state) => state.songs.songs);
+  const songStatus = useSelector((state) => state.songs.status);
 
   useEffect(() => {
     if (songStatus === 'idle') {
@@ -16,13 +29,13 @@ const App = () => {
   }, [songStatus, dispatch]);
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-900 via-black to-purple-500 text-white flex flex-col items-center p-6">
-      <h1 className="text-5xl font-extrabold mb-10">Song Manager</h1>
-      <div className="w-full max-w-2xl">
+    <AppContainer>
+      <Title>Song Manager</Title>
+      <div css={css`width: 100%; max-width: 32rem;`}>
         <AddSongForm />
         <SongList songs={songs} />
       </div>
-    </div>
+    </AppContainer>
   );
 };
 
