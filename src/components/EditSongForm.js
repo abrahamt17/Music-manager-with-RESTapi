7f1527187f480f8b1c694a082df8c3e69ab0e0ad
abrahamt17 <abrahamtikuye@gmail.com>
2025-03-30 14:20:45 +0200

    debug

--- src/components/EditSongForm.js
+++ src/components/EditSongForm.js
@@ -1,29 +1,29 @@
-// import React from 'react';
-// import { useDispatch } from 'react-redux';
-// import { deleteSong, updateSong } from '../songsSlice';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteSong, updateSong } from '../songsSlice';
 
-// const updateSong = ({ songs }) => {
-//   const dispatch = useDispatch();
-//   const [title, setTitle] = useState('');
+const updateSong = ({ songs }) => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
 
-//   const handleUpdate = (id) => {
-//     dispatch(updateSong(id));
-//   };
+  const handleUpdate = (id) => {
+    dispatch(updateSong(id));
+  };
 
-//   return (
-//     <ul>
-//       {songs.map(song => (
-//         <li key={song.id}  onClick={() => handleUpdate(song.id)}  >
-//           {song.title }
+  return (
+    <ul>
+      {songs.map(song => (
+        <li key={song.id}  onClick={() => handleUpdate(song.id)}  >
+          {song.title }
 
-//           {/* <button onClick={() => handleUpdate(song.id)}>Edit</button> */}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
+          <button onClick={() => handleUpdate(song.id)}>Edit</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-// export default updateSong;
+export default updateSong;
 
 
 
